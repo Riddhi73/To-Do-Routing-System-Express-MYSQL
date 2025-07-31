ccfee44367a7679c5cc493a9f19d03fee8b19f62
Riddhi73 <riddhimandebnath4260@gmail.com>
2025-08-01 00:08:05 +0600

    added put method

--- handlers/routes/createHandler.js
+++ handlers/routes/createHandler.js
@@ -83,4 +83,57 @@ handler._create.get = (reqProp, callback) => {
   }
 };
 
+handler._create.put = (reqProp, callback) => {
+    const id =
+        typeof reqProp.body.id === "number" && reqProp.body.id > 0
+        ? reqProp.body.id
+        : false;
+    const title =
+        typeof reqProp.body.title === "string" &&
+        reqProp.body.title.trim().length > 0
+        ? reqProp.body.title.trim()
+        : false;
+    const description =
+        typeof reqProp.body.description === "string" &&
+        reqProp.body.description.trim().length > 0
+        ? reqProp.body.description.trim()
+        : false;
+    const date =
+        typeof reqProp.body.date === "string" && reqProp.body.date.trim().length > 0
+        ? reqProp.body.date.trim()
+        : false;
+    
+    if (id && (title || description || date)) {
+        lib.read("data", "data", (err, data) => {
+        if (!err && data) {
+            let todo = JSON.parse(JSON.stringify(data));
+            const todoIndex = todo.findIndex((item) => item.id === id);
+            if (todoIndex !== -1) {
+            if (title) todo[todoIndex].title = title;
+            if (description) todo[todoIndex].description = description;
+            if (date) todo[todoIndex].date = date;
+    
+            lib.update("data", "data", todo, (err) => {
+                if (!err) {
+                callback(200, { message: "To-do updated successfully" });
+                } else {
+                callback(500, { error: `Error updating resource ${err}` });
+                }
+            });
+            } else {
+            callback(404, { error: "To-do not found" });
+            }
+        } else {
+            callback(500, { error: `Error reading data ${err}` });
+        }
+        });
+    } else {
+        callback(400, {
+        error: `Missing required fields: ${!id ? "id" : ""} ${
+            !title ? "title" : ""
+        } ${!description ? "description" : ""} ${!date ? "date" : ""}`,
+        });
+    }
+}
+
 module.exports = handler;
