1a0c4c5572dfdaa2f18c90d9814a058ce4a3bad5
Riddhi73 <riddhimandebnath4260@gmail.com>
2025-08-01 00:16:46 +0600

    added delete method

--- handlers/routes/createHandler.js
+++ handlers/routes/createHandler.js
@@ -136,4 +136,34 @@ handler._create.put = (reqProp, callback) => {
     }
 }
 
+handler._create.delete = (reqProp, callback) => {
+    const id =
+        typeof reqProp.queryStringObj.id === "string" &&
+        !isNaN(parseInt(reqProp.queryStringObj.id)) &&
+        parseInt(reqProp.queryStringObj.id) > 0
+            ? parseInt(reqProp.queryStringObj.id)
+            : false;
+    if (id) {
+        lib.read("data", "data", (err, data) => {
+            if (!err && data) {
+                let todo = JSON.parse(JSON.stringify(data));
+                const todoIndex = todo.findIndex((item) => item.id === id);
+                if (todoIndex !== -1) {
+                    todo.splice(todoIndex, 1);
+                    lib.update("data", "data", todo, (err) => {
+                        if (!err) {
+                            callback(200, { message: "To-do deleted successfully" });
+                        } else {
+                            callback(500, { error: `Error deleting resource ${err}` });
+                        }
+                    });
+                } else {
+                    callback(404, { error: "To-do not found" });
+                }
+            } else {
+                callback(500, { error: `Error reading data ${err}` });
+            }
+        });
+    }
+}
 module.exports = handler;
