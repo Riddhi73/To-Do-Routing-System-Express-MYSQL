a2ca66b7892d06b0f718eb247ee70c14fab8f218
Riddhi73 <riddhimandebnath4260@gmail.com>
2025-09-06 17:59:00 +0600

    Tested Sample Route

--- handlers/routes/sampleHandler.js
+++ handlers/routes/sampleHandler.js
@@ -1,6 +1,14 @@
+const db = require("../../data/database");
+
 // sampleHandler.js
-const sampleHandler = (req, res) => {
-  console.log("Sample handler is working");
-  res.status(200).json({ message: "This is a sample URL" });
+const sampleHandler = async (req, res) => {
+  try {
+    const [results] = await db.query("SELECT * FROM authors");
+    res.status(200).json(results);
+  } catch (err) {
+    console.error("Error reading data:", err);
+    res.status(500).json({ error: "Error Reading Data" });
+  }
 };
+
 module.exports = sampleHandler;
