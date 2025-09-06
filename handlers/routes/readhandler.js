b45c5e0778a96c45d2786d7214eaa3979bb21094
Riddhi73 <riddhimandebnath4260@gmail.com>
2025-09-06 19:24:35 +0600

    Added Read Operation

--- handlers/routes/readhandler.js
+++ handlers/routes/readhandler.js
@@ -1,30 +1,38 @@
 // readHandler.js
 const lib = require("../../lib/data");
+const db = require("../../data/database");
 
-const readAll = (req, res) => {
-  lib.read("data", "data", (err, data) => {
-    if (!err && data) {
-      const todos = typeof data === "string" ? JSON.parse(data) : data;
-      return res.status(200).json(todos);
-    }
-    return res.status(500).json({ error: "Error reading data" });
-  });
+const readAll = async (req, res) => {
+  try {
+    const [results] = await db.query(
+      "SELECT * FROM contents INNER JOIN authors ON contents.authors_id = authors.id"
+    );
+    res.status(200).json(results);
+  } catch (err) {
+    console.error("Error reading data:", err);
+    res.status(500).json({ error: "Error Reading Data" });
+  }
 };
 
-const readSingle = (req, res) => {
+const readSingle = async (req, res) => {
   const id = parseInt(req.params.id);
   if (isNaN(id) || id <= 0) {
     return res.status(400).json({ error: "Invalid ID format" });
   }
-  lib.read("data", "data", (err, data) => {
-    if (!err && data) {
-      const todos = typeof data === "string" ? JSON.parse(data) : data;
-      const todo = todos.find((item) => item.id === id);
-      if (todo) return res.status(200).json(todo);
-      return res.status(404).json({ error: "To-do not found" });
+  try {
+    const [results] = await db.query(
+      `SELECT * FROM contents INNER JOIN authors ON contents.authors_id = authors.id WHERE authors.id = ?`,
+      [req.params.id]
+    );
+    if (results.length === 0) {
+      res.status(500).json({ error: "No Data" });
+    } else {
+      res.status(200).json(results);
     }
-    return res.status(500).json({ error: "Error reading data" });
-  });
+  } catch (err) {
+    console.error("Error reading data:", err);
+    res.status(500).json({ error: "Error Reading Data" });
+  }
 };
 
 module.exports = { readAll, readSingle };
