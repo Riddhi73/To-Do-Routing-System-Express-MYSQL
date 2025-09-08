980fe621fea5bcf7f55266249b0400724951dd5f
Riddhi73 <riddhimandebnath4260@gmail.com>
2025-09-08 11:17:26 +0600

    Checked Sample Route

--- handlers/routes/sampleHandler.js
+++ handlers/routes/sampleHandler.js
@@ -3,8 +3,8 @@ const db = require("../../data/database");
 // sampleHandler.js
 const sampleHandler = async (req, res) => {
   try {
-    const [results] = await db.query("SELECT * FROM authors");
-    res.status(200).json(results);
+    const authors = await db.getDb().collection("authors").find().toArray();
+    res.status(200).json(authors);
   } catch (err) {
     console.error("Error reading data:", err);
     res.status(500).json({ error: "Error Reading Data" });
