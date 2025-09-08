b7b69357957b7098881272ae6374d629bf7a1844
Riddhi73 <riddhimandebnath4260@gmail.com>
2025-09-08 15:06:44 +0600

    Read data

--- handlers/routes/readhandler.js
+++ handlers/routes/readhandler.js
@@ -4,10 +4,14 @@ const db = require("../../data/database");
 
 const readAll = async (req, res) => {
   try {
-    const [results] = await db.query(
-      "SELECT * FROM contents INNER JOIN authors ON contents.authors_id = authors.id"
-    );
-    res.status(200).json(results);
+    const results = await db.getDb().collection('contents').find().toArray();
+    // console.log(results);
+    const filteredResult = results.map((item) => ({
+      name: item.author.name,
+      title: item.title,
+      description: item.description
+    }))
+    res.status(200).json(filteredResult);
   } catch (err) {
     console.error("Error reading data:", err);
     res.status(500).json({ error: "Error Reading Data" });
